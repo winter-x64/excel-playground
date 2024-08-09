3ca3f80c20115e7901e39efd8fb4cc4877792550
winter_x64 <itswinterx64@gmail.com>
2024-08-09 08:21:06 +0530

    Update file

--- sheet/spliter.py
+++ sheet/spliter.py
@@ -5,4 +5,4 @@ df = pd.read_excel("filename.xlsx", sheet_name=None)
 
 # Split the sheets into different files
 for sheet_name, sheet_data in df.items():
-        sheet_data.to_excel(f"{sheet_name}.xlsx", index=False)
+    sheet_data.to_excel(excel_writer=f"{sheet_name}.xlsx", index=False)
